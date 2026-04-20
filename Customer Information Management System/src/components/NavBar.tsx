62d23c1a06347f68a1d6f7574c78a14cae368a5a
AbaclodSIT-tj <2025227@s.ubaguio.edu>
2026-04-20 11:42:39 +0800

    cleaning for now

--- Customer Information Management System/src/components/NavBar.tsx
+++ Customer Information Management System/src/components/NavBar.tsx
@@ -1,8 +1,8 @@
 const NavBar=()=>{
     return (
-        <nav style={{padding: 12, borderBottom: '1px solid #ddd'}}>
-            <strong>Customer Management</strong>
-        </nav>
+        <>
+        
+        </>
     )
 }
 export default NavBar;
